flamingradian/fresh-movers-co

commit 4ff51e26991ba2cc557b1cc2e54c4a95cbf53aab

scripts: crate: do not indicate box as selectable when dropping another
Similarly to the workaround with the actual selection of the box, the

Also do not check if the box is selected, because it implies that a box
is selected.

Fresh Movers Co/Scripts/Crate.gd
@@ -53,12 +53,12 @@ func edit_mode_exit():
 
 func _on_mouse_entered():
 	isHovering = true
-	if itemManager.GetIsItemSelected() == false and isInTruck == false and isSelected == false:
+	if itemManager.GetIsItemSelected() == false and itemManager.GetDeselectOnly() == false and and isInTruck == false:
 		self.set_selected_texture()
 
 func _on_mouse_exited():
 	isHovering = false
-	if itemManager.GetIsItemSelected() == false and isInTruck == false and isSelected == false:
+	if itemManager.GetIsItemSelected() == false and itemManager.GetDeselectOnly() == false and isInTruck == false:
 		self.set_default_texture()
 
 func on_mouse_pressed():

commit 3181166f6a97aa20b0bd27886a4f16e4003e6143

scripts: crate: remove extra and
This should not be here. Remove.

Fixes: 4ff51e26991b ("scripts: crate: do not indicate box as selectable when dropping another")

Fresh Movers Co/Scripts/Crate.gd
@@ -53,7 +53,7 @@ func edit_mode_exit():
 
 func _on_mouse_entered():
 	isHovering = true
-	if itemManager.GetIsItemSelected() == false and itemManager.GetDeselectOnly() == false and and isInTruck == false:
+	if itemManager.GetIsItemSelected() == false and itemManager.GetDeselectOnly() == false and isInTruck == false:
 		self.set_selected_texture()
 
 func _on_mouse_exited():