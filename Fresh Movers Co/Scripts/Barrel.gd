49be397aa03377907e0aa9527d0a3f261ffd089e
pt50hgm <pt50hgm@gmail.com>
2023-09-27 12:49:36 -0400

    Added barrel
    Created crate and barrel which inherit from item
    Added sprites and attached them

---
+++ Fresh Movers Co/Scripts/Barrel.gd
@@ -0,0 +1,27 @@
+extends "res://Scripts/Item.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func set_default_texture():
+	$NinePatchRect.texture = load("res://Sprites/barrel.png")
+
+# Helper to set the texture when this is in an invalid position.
+func set_invalid_texture():
+	$NinePatchRect.texture = load("res://Sprites/barrel_invalid.png")
+
+# Helper to set the texture when this is selected.
+func set_selected_texture():
+	$NinePatchRect.texture = load("res://Sprites/barrel_selected.png")
